ead07882c832eee92518644368a45cb16d84aef6
Brian Mutinda <mutindabrian26@gmail.com>
2023-02-27 13:09:59 +0300

    test add order screen

--- test/order/add_order_screen_test.dart
+++ test/order/add_order_screen_test.dart
@@ -12,6 +12,12 @@ void main() {
     expect(find.text('Drop-off Point'), findsOneWidget);
     expect(find.text('Est. Weight (KGs)'), findsOneWidget);
     expect(find.text('Delivery Instructions'), findsOneWidget);
+    expect(find.text('Confirm & order & trip'), findsOneWidget);
+
+    expect(
+        find.byWidget(TextFormField(), skipOffstage: false), findsNWidgets(4));
+    expect(find.byWidget(TextButton(), skipOffstage: false), findsOneWidget);
+
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
