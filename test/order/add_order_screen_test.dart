b2af72a4d125e060f901272b7dc0ca8b78e7ba62
Brian Mutinda <mutindabrian26@gmail.com>
2023-02-27 13:07:17 +0300

    test add order screen

---
+++ test/order/add_order_screen_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_app/main.dart';
+
+void main() {
+  testWidgets('Test add order screen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    expect(find.text('Fill Below to Order'), findsOneWidget);
+    expect(find.text('Pickup Point'), findsOneWidget);
+    expect(find.text('Drop-off Point'), findsOneWidget);
+    expect(find.text('Est. Weight (KGs)'), findsOneWidget);
+    expect(find.text('Delivery Instructions'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+  });
+}
